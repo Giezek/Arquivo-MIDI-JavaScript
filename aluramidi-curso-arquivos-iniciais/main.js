d8b40e286da4b830723fe2435fb3b7dc4b590630
Giezek <104865473+Giezek@users.noreply.github.com>
2023-09-25 09:00:03 -0300

    Atualização de arquivos.

---
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -0,0 +1,38 @@
+alert('Bom dia, 2 º D!');
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
+}
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+function tocaSomclap () {
+    document.querySelector('#som_tecla_clap').play();
+}
+document.querySelector('.tecla_clap').onclick = tocaSomclap;
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
+}
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
+}
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
+}
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
+}
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
+}
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
+}
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
+}
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
