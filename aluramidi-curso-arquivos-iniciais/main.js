a27d2f104027ef3b845c95cf97e7883796cae0bd
Giezek <104865473+Giezek@users.noreply.github.com>
2023-10-11 11:48:34 +0000

    tom

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -30,9 +30,9 @@ document.querySelector('.tecla_tic').onclick = tocaSomtic;
 function tocaSomtic () {
     document.querySelector('#som_tecla_tic').play();
 }
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+document.querySelector('.tecla_tom').onclick = tocaSomtom;
+function tocaSomtom () {
+    document.querySelector('#som_tecla_tom').play();
 }
 document.querySelector('.tecla_pom').onclick = tocaSomPom;
 
