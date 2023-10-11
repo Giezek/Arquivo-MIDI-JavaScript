ab70ba3a000109cf45df39dfdb6f17492847f18a
Giezek <104865473+Giezek@users.noreply.github.com>
2023-10-11 11:41:20 +0000

    splash

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -14,9 +14,9 @@ document.querySelector('.tecla_puff').onclick = tocaSompuff;
 function tocaSompuff () {
     document.querySelector('#som_tecla_puff').play();
 }
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+document.querySelector('.tecla_splash').onclick = tocaSomSplash;
+function tocaSomSplash () {
+    document.querySelector('#som_tecla_splash').play();
 }
 document.querySelector('.tecla_pom').onclick = tocaSomPom;
 function tocaSomPom () {
