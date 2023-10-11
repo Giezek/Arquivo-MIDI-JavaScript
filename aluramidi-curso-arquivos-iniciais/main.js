d3328e271871f9e3a24ed4acbd1bf9a1fb231543
Giezek <104865473+Giezek@users.noreply.github.com>
2023-10-11 11:38:52 +0000

    correção som teclas

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -2,17 +2,17 @@ alert('Bom dia, 2 º C!');
 function tocaSomPom () {
     document.querySelector('#som_tecla_pom').play();
 }
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+document.querySelector('.tecla_clap').onclick = tocaSomclap;
 function tocaSomclap () {
     document.querySelector('#som_tecla_clap').play();
 }
-document.querySelector('.tecla_clap').onclick = tocaSomclap;
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+document.querySelector('.tecla_tim').onclick = tocaSomtim;
+function tocaSomtim () {
+    document.querySelector('#som_tecla_tim').play();
 }
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+document.querySelector('.tecla_puff').onclick = tocaSompuff;
+function tocaSompuff () {
+    document.querySelector('#som_tecla_puff').play();
 }
 document.querySelector('.tecla_pom').onclick = tocaSomPom;
 function tocaSomPom () {
